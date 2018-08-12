749203b4d24331a6c581a969b62b8ca94e11eceb
Christopher Burt <cwadeburt@gmail.com>
2018-08-12 18:07:51 -0500

    variable declaration

---
+++ Documents/netbeans_Java_Files/Burt_C_2/src/burt_c_2/Burt_C_2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package burt_c_2;
+
+/**
+ *
+ * @author cwade
+ */
+public class Burt_C_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        double tip_15, tip_18, tip_20; 
+        double bill_amount, tip_total, final_total;
+        
+        tip_15 = .15;
+        tip_18 = .18;
+        tip_20 = .20;
+        
+        tip_total = bill_amount * 
+        
+               
+    }
+    
+}
