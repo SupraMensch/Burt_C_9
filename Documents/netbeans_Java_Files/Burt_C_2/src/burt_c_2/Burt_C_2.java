cefcee441eac8d6db61aa32c61358b233f64fded
Christopher Burt <cwadeburt@gmail.com>
2018-08-23 21:21:25 -0500

    Edits and adjustments final(I think)

--- Documents/netbeans_Java_Files/Burt_C_2/src/burt_c_2/Burt_C_2.java
+++ Documents/netbeans_Java_Files/Burt_C_2/src/burt_c_2/Burt_C_2.java
@@ -1,31 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Burt, Christopher Project#2
+
+// This program prompts the user for the cost of a meal and then calculates and displays 
+// three different percentage tip types
+
 package burt_c_2;
 
-/**
- *
- * @author cwade
- */
-public class Burt_C_2 {
+import java.util.Scanner; 
+public class Burt_C_2
+{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+        public static void main(String[] args) 
+    {
+			
+        double tip_15, tip_18, tip_20;// variable declaration		 
+        double bill_Amount;
+        double tip_15WithBill, tip_18WithBill, tip_20WithBill;
+       
+        Scanner userInput = new Scanner(System.in);
+        //Gather user input and display
+        System.out.print("Please enter the bill total for your meal: $ ");
+        bill_Amount = userInput.nextDouble();
         
-        double tip_15, tip_18, tip_20; 
-        double bill_amount, tip_total, final_total;
+        tip_15 = .15 * bill_Amount;
+        tip_15WithBill = tip_15 + bill_Amount;
+        System.out.printf("\nThe tip at 15 percent is: $ %4.2f \n", tip_15 );
+        System.out.printf("The bill with a 15 percent tip is: $ %4.2f \n \n", tip_15WithBill );
         
-        tip_15 = .15;
-        tip_18 = .18;
-        tip_20 = .20;
         
-        tip_total = bill_amount * 
+        tip_18 = .18 * bill_Amount;
+        tip_18WithBill = tip_18 + bill_Amount;
+        System.out.printf("\nThe tip at 18 percent is: $ %4.2f \n", tip_18 );
+        System.out.printf("The bill with an 18 percent tip is: $%4.2f \n", tip_18WithBill );
         
-               
+        
+        tip_20 = .20 * bill_Amount;
+        tip_20WithBill = tip_20 + bill_Amount;
+        System.out.printf("\nThe tip at 20 percent is: $ %4.2f \n", tip_20 );
+        System.out.printf("The bill with a 20 percent tip is: $ %4.2f \n", tip_20WithBill );        
+      
+            
+        
+              
     }
     
 }
+
+
