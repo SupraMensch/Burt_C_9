fd33b4d78690bc783c91c8540382f4de4ce913bc
Christopher Burt <cwadeburt@gmail.com>
2018-08-23 21:27:01 -0500

    blank space removed and additional formatting and escape sequences added

--- Documents/netbeans_Java_Files/Burt_C_2/src/burt_c_2/Burt_C_2.java
+++ Documents/netbeans_Java_Files/Burt_C_2/src/burt_c_2/Burt_C_2.java
@@ -8,10 +8,8 @@ package burt_c_2;
 import java.util.Scanner; 
 public class Burt_C_2
 {
-
         public static void main(String[] args) 
     {
-			
         double tip_15, tip_18, tip_20;// variable declaration		 
         double bill_Amount;
         double tip_15WithBill, tip_18WithBill, tip_20WithBill;
@@ -22,27 +20,20 @@ public class Burt_C_2
         bill_Amount = userInput.nextDouble();
         
         tip_15 = .15 * bill_Amount;
-        tip_15WithBill = tip_15 + bill_Amount;
-        System.out.printf("\nThe tip at 15 percent is: $ %4.2f \n", tip_15 );
+        tip_15WithBill = tip_15 + bill_Amount; // initialize combined tip percentage and original bill amount
+        System.out.printf("\nThe tip at 15 percent is: $ %4.2f \n", tip_15 );  // chose 4.3 for average meal purchase& payment practicality
         System.out.printf("The bill with a 15 percent tip is: $ %4.2f \n \n", tip_15WithBill );
-        
         
         tip_18 = .18 * bill_Amount;
         tip_18WithBill = tip_18 + bill_Amount;
         System.out.printf("\nThe tip at 18 percent is: $ %4.2f \n", tip_18 );
         System.out.printf("The bill with an 18 percent tip is: $%4.2f \n", tip_18WithBill );
         
-        
         tip_20 = .20 * bill_Amount;
         tip_20WithBill = tip_20 + bill_Amount;
         System.out.printf("\nThe tip at 20 percent is: $ %4.2f \n", tip_20 );
         System.out.printf("The bill with a 20 percent tip is: $ %4.2f \n", tip_20WithBill );        
-      
-            
-        
-              
     }
-    
-}
+}  
 
 
