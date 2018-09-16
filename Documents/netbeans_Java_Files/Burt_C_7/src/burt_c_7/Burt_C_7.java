2c25c32913b56bc2f688fdc76406df1efdbedfe7
Christopher Burt <cwadeburt@gmail.com>
2018-09-15 21:48:59 -0500

    Final

--- Documents/netbeans_Java_Files/Burt_C_7/src/burt_c_7/Burt_C_7.java
+++ Documents/netbeans_Java_Files/Burt_C_7/src/burt_c_7/Burt_C_7.java
@@ -1,3 +1,5 @@
+// Christopher Burt
+// Project # 7
 // This program displays the average of a set of test scores 
 package burt_c_7;
 
@@ -5,27 +7,26 @@ import javax.swing.JOptionPane;
 
 public class Burt_C_7
 {
-
     public static void main(String[] args) 
     {
         String numberOfScores, testScore;  
-        int scores_Total, Score_Count;
+        int scores_Total, Score_Count, total;
         double actualTotal, testAverage;
-        
-        numberOfScores = JOptionPane.showInputDialog("Please enter The number of test entries you wish to enter: ");
+        //Get number of test entries
+        numberOfScores = JOptionPane.showInputDialog("Please enter the number of scores you have to enter: ");
         scores_Total = Integer.parseInt(numberOfScores);
-        testAverage = 0;
+        // initialize global total for later average calculation 
+        total = 0;
+        // Gather test score info for # of test entries entered
         for ( Score_Count = 1; Score_Count <= scores_Total; Score_Count++ )
         {
             testScore = JOptionPane.showInputDialog(null, "Please enter test score # " + Score_Count );
             actualTotal = Double.parseDouble(testScore);
-            testAverage = (actualTotal / Score_Count );     
+            total +=  actualTotal;
         }
-   
-        JOptionPane.showMessageDialog (null, "The average of the test scores is: " + testAverage);
-   
-
-            
-    }
-    
+        // Calculate and display the test average
+        testAverage = (total / scores_Total );
+        JOptionPane.showMessageDialog (null, "The average of the test scores you've entered is: " + testAverage);   
+    }   
 }
+
