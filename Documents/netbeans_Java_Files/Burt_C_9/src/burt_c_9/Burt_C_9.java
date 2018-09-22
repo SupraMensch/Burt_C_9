c8ccffba615b689755cade1f6f6e17acf5b0e1d5
SupraMensch <cwadeburt@gmail.com>
2018-09-22 14:13:49 -0500

    celsius to fahrenheit conversion

--- Documents/netbeans_Java_Files/Burt_C_9/src/burt_c_9/Burt_C_9.java
+++ Documents/netbeans_Java_Files/Burt_C_9/src/burt_c_9/Burt_C_9.java
@@ -9,20 +9,21 @@ package burt_c_9;
 
 // This program converts celsius to Farenheit and displays the result as a table
 
-
-public class Burt_C_9 {
+import java.util.Scanner;
+public class Burt_C_9
+{
 
     // 1. Create a method that accepts the Celsius temperature as an argument and returns the Fahrenheit temperature.
     //2. In your main method, write a loop that that calls the method and passes it temperatures from 0 to 100 in increments of 10 (0, 10, 20, 30, etc.).
     //3. In this loop, display the Celsius and Fahrenheit equivalent as a table.
     public static void main(String[] args)
     {
-        // TODO code application logic here
-        // this is a test
-        
-        //try meeeeeeeee biatchchchchc;
-        
-        // time to do another test
+        double celsius, fahrenheit;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the temperature in Celsius:");
+        celsius = input.nextDouble();
+        fahrenheit = celsius * 1.8 +32;
+        System.out.println("The temperature in Fahrenheit is:" + fahrenheit);
         
         
         
