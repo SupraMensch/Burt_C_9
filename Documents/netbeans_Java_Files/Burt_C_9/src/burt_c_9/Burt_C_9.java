640029eea8f50a7aaa99fe225760942b5ff8d8aa
Christopher Burt <cwadeburt@gmail.com>
2018-09-22 12:13:08 -0500

    first draft

---
+++ Documents/netbeans_Java_Files/Burt_C_9/src/burt_c_9/Burt_C_9.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package burt_c_9;
+
+/**
+ *
+ * @author cwade
+ */
+public class Burt_C_9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
