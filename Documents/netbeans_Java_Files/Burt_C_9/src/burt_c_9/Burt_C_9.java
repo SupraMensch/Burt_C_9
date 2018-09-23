e728f502546865510ed7a418d998d3e49e553c8e
SupraMensch <cwadeburt@gmail.com>
2018-09-23 14:55:46 -0500

    final draft

--- Documents/netbeans_Java_Files/Burt_C_9/src/burt_c_9/Burt_C_9.java
+++ Documents/netbeans_Java_Files/Burt_C_9/src/burt_c_9/Burt_C_9.java
@@ -1,36 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package burt_c_9;
 // Christopher Burt
 // Project # 9
 
 // This program converts celsius to Farenheit and displays the result as a table
 
-import java.util.Scanner;
+package burt_c_9;
+
+ // 1. Create a method that accepts the Celsius temperature as an argument and returns the Fahrenheit temperature.
+ //2. In your main method, write a loop that that calls the method and passes it temperatures from 0 to 100 in increments of 10 (0, 10, 20, 30, etc.).
+ //3. In this loop, display the Celsius and Fahrenheit equivalent as a table.
 public class Burt_C_9
-{
-
-    // 1. Create a method that accepts the Celsius temperature as an argument and returns the Fahrenheit temperature.
-    //2. In your main method, write a loop that that calls the method and passes it temperatures from 0 to 100 in increments of 10 (0, 10, 20, 30, etc.).
-    //3. In this loop, display the Celsius and Fahrenheit equivalent as a table.
-    public static void main(String[] args)
-    {
-        double celsius, fahrenheit;
-         
         
-
-    }  
-    public static double celsiusToFarenheit(double fahrenheit)
+{
+    
+    //Main method with loop that increments values 0 to 100
+       public static void main(String[] args)
     {
-        double celsius;
-        Scanner input = new Scanner(System.in);
-        System.out.print("Please enter the temperature in Celsius:");
-        celsius = input.nextDouble();
-        fahrenheit = celsius * 1.8 +32;
-        System.out.println("The temperature in Fahrenheit is:" + fahrenheit);
+       double celsius;
+       //loop to increment by 10 for celsius
+       System.out.println("Temperature Celsius:" + "                             " + "Temperature Fahrenheit:\n");
+       for (double i = 0; i <= 100; i+=10)
+           celsiusToFahrenheit(i);
+  
+    } 
+    
+    // Celsius to Fahrenheit converter Method
+    public static double celsiusToFahrenheit(double celsius)
+    {
+         double celsiusToFahrenheit;
+        celsiusToFahrenheit = celsius * 1.8 + 32;
+        System.out.println(celsius+ "                                              " + celsiusToFahrenheit);
         return celsius;
     }
     
