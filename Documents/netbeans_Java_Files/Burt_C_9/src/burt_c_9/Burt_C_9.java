600f80353c568555e72aa2ec324ef68a28065f99
SupraMensch <cwadeburt@gmail.com>
2018-09-23 15:18:35 -0500

    final draft2

--- Documents/netbeans_Java_Files/Burt_C_9/src/burt_c_9/Burt_C_9.java
+++ Documents/netbeans_Java_Files/Burt_C_9/src/burt_c_9/Burt_C_9.java
@@ -8,8 +8,8 @@ package burt_c_9;
  // 1. Create a method that accepts the Celsius temperature as an argument and returns the Fahrenheit temperature.
  //2. In your main method, write a loop that that calls the method and passes it temperatures from 0 to 100 in increments of 10 (0, 10, 20, 30, etc.).
  //3. In this loop, display the Celsius and Fahrenheit equivalent as a table.
-public class Burt_C_9
-        
+
+public class Burt_C_9     
 {
     
     //Main method with loop that increments values 0 to 100
@@ -20,15 +20,14 @@ public class Burt_C_9
        System.out.println("Temperature Celsius:" + "                             " + "Temperature Fahrenheit:\n");
        for (double i = 0; i <= 100; i+=10)
            celsiusToFahrenheit(i);
-  
-    } 
+      } 
     
     // Celsius to Fahrenheit converter Method
     public static double celsiusToFahrenheit(double celsius)
     {
-         double celsiusToFahrenheit;
+        double celsiusToFahrenheit;
         celsiusToFahrenheit = celsius * 1.8 + 32;
-        System.out.println(celsius+ "                                              " + celsiusToFahrenheit);
+        System.out.printf("%3.2f                                            %3.2f\n ", celsius, celsiusToFahrenheit);
         return celsius;
     }
     
